01e099247e53faf94cdb2f1ea784a2c7f5fb82bc
Giovanni <cascafico@gmail.com>
2021-02-23 13:50:17 +0100

    Update CAI_mods.sh

--- CAI_mods.sh
+++ CAI_mods.sh
@@ -4,7 +4,6 @@ T0=`date -d "$INTERVAL" '+%Y-%m-%dT%H:%M:%SZ'`
 T1=`date                '+%Y-%m-%dT%H:%M:%SZ'`
 AREACODE=3600179296
 
-curl -G 'http://overpass-api.de/api/interpreter' --data-urlencode 'data=[out:xml][timeout:300][adiff:"'$T0'","'$T1'"];area('$AREACODE')->.searchArea;(relation["operator"="Club Alpino Italiano"](area.searchArea);rel
-ation["operator"="CAI"](area.searchArea););(._;>;);out meta geom;' > CAI_mods.osm
+curl -G 'http://overpass-api.de/api/interpreter' --data-urlencode 'data=[out:xml][timeout:300][adiff:"'$T0'","'$T1'"];area('$AREACODE')->.searchArea;(relation["operator"="Club Alpino Italiano"](area.searchArea);relation["operator"="CAI"](area.searchArea););(._;>;);out meta geom;' > CAI_mods.osm
 
 cat CAI_mods.osm | grep "relation id" | awk -F\" '{print "https://pewu.github.io/osm-history/#/relation/"$2}' | sort -u > last_changes_relations
