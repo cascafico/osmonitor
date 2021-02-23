e82257413eb2d896958d571dbccc3af22bddcca4
Giovanni <cascafico@gmail.com>
2021-02-23 21:39:52 +0100

    Update CAI_mods.sh

--- CAI_mods.sh
+++ CAI_mods.sh
@@ -7,5 +7,5 @@ AREACODE=3600179296
 #curl -G 'http://overpass-api.de/api/interpreter' --data-urlencode 'data=[out:xml][timeout:300][adiff:"'$T0'","'$T1'"];area('$AREACODE')->.searchArea;(relation["operator"="Club Alpino Italiano"](area.searchArea);relation["operator"="CAI"](area.searchArea););(._;>;);out meta geom;' > CAI_mods.osm
 curl -G 'http://overpass-api.de/api/interpreter' --data-urlencode 'data=[out:xml][timeout:300][adiff:"'$T0'","'$T1'"];area('$AREACODE')->.searchArea;(relation["operator"="Club Alpino Italiano"](area.searchArea);relation["operator"="CAI"](area.searchArea););(._;>;);out;' > CAI_mods.osm
 
-#cat CAI_mods.osm | grep "relation id" | awk -F\" '{print "https://pewu.github.io/osm-history/#/relation/"$2}' | sort -u > last_changes_relations.html
+cat CAI_mods.osm | grep "relation id" | awk -F\" '{print "https://pewu.github.io/osm-history/#/relation/"$2}' | sort -u > last_changes_relations_meta.html
 cat CAI_mods.osm | grep "relation id" | awk -F\" '{print "<a href=https://pewu.github.io/osm-history/#/relation/"$2">"$2"</A>"}' | sort -u > last_changes_relations.html
