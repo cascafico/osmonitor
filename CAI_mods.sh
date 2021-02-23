576e1146e82d1ea376c52895f99d04eade264f52
Giovanni <cascafico@gmail.com>
2021-02-23 11:46:24 +0100

    Update CAI_mods.sh

--- CAI_mods.sh
+++ CAI_mods.sh
@@ -1,1 +1,5 @@
-https://overpass-api.de/api/interpreter?data=%5Bout%3Axml%5D%5Btimeout%3A300%5D%5Badiff%3A%222021%2D02%2D15T00%3A00%3A00Z%22%2C%222021%2D02%2D23T00%3A00%3A00Z%22%5D%3Barea%283600179296%29%2D%3E%2EsearchArea%3B%28relation%5B%22route%22%3D%22hiking%22%5D%5B%22operator%22%7E%22club%20alpino%20italiano%22%2Ci%5D%28area%2EsearchArea%29%3B%29%3Bout%3B%0A
+# based on query
+# https://overpass-turbo.eu/s/142m
+
+
+wget -O CAI_mods.osm "https://overpass-api.de/api/interpreter?data=%5Bout%3Axml%5D%5Btimeout%3A300%5D%5Badiff%3A%222021%2D02%2D15T00%3A00%3A00Z%22%2C%222021%2D02%2D23T00%3A00%3A00Z%22%5D%3Barea%283600179296%29%2D%3E%2EsearchArea%3B%28relation%5B%22route%22%3D%22hiking%22%5D%5B%22operator%22%7E%22club%20alpino%20italiano%22%2Ci%5D%28area%2EsearchArea%29%3B%29%3Bout%3B%0A"
