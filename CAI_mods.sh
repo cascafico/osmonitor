0ad103f8c129e8934652df11f13d0bc4e463b376
Giovanni <cascafico@gmail.com>
2021-02-24 13:28:39 +0000

    Update CAI_mods.sh

--- CAI_mods.sh
+++ CAI_mods.sh
@@ -1,6 +1,5 @@
 ######## start customization
-#AREACODE=3600000000 + <see areacodes file>
-AREACODE=3600179296
+#AREACODE=3600000000 + <see areacodes in actions file>
 AREACODE= $1
 INTERVAL="yesterday"
 #INTERVAL="1 hour ago"
