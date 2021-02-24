0b43a05316a58e54bb9d381b158ab3a2b17634a9
Giovanni <cascafico@gmail.com>
2021-02-24 13:27:20 +0000

    Update CAI_mods.sh

--- CAI_mods.sh
+++ CAI_mods.sh
@@ -13,14 +13,9 @@ T1=`date                '+%Y-%m-%dT%H:%M:%SZ'`
 IERI=`date -d "$INTERVAL" '+%Y-%m-%d'`
 OGGI=`date +"%Y-%m-%d"`
 
-REGIONEQ=$REGIONE'adiff'$OGGI'.xml'
-
 
 # extracting overpass adiff differences
 curl -G 'http://overpass-api.de/api/interpreter' --data-urlencode 'data=[out:xml][timeout:300][adiff:"'$T0'","'$T1'"];area('$AREACODE')->.searchArea;(relation["operator"="Club Alpino Italiano"](area.searchArea);relation["operator"="CAI"](area.searchArea););(._;>;);out meta geom;' > $AREACODE'.osm'
-
-# parsing involved changeset(s)
-cat $AREACODE'.osm' | grep "$IERI\|$OGGI" | grep changeset | awk ' { print substr($0,index($0, "changeset")+11,8) }' | sort -u > $AREACODE'changeset.lst'
 
 echo "<HTML><BODY>Monitor process run on $T1<BR>Latest 24h changesets:<BR>" > $AREACODE'changeset.html'
 cat $AREACODE'.osm' | grep "$IERI\|$OGGI" | grep changeset | awk ' { $changeset=substr($0,index($0, "changeset")+11,8); print "<A HREF=https://overpass-api.de/achavi/?changeset="$changeset">"$changeset"</A><BR>" }' | sort -u >> $AREACODE'changeset.html'
