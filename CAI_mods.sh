ce71e920a50829355cc7ccc60700d22166917b80
Giovanni <cascafico@gmail.com>
2021-02-24 09:33:50 +0000

    Update CAI_mods.sh

--- CAI_mods.sh
+++ CAI_mods.sh
@@ -24,7 +24,7 @@ curl -G 'http://overpass-api.de/api/interpreter' --data-urlencode 'data=[out:xml
 cat $REGIONEQ | grep "$IERI\|$OGGI" | grep changeset | awk ' { print substr($0,index($0, "changeset")+11,8) }' | sort -u > $REGIONE'changeset.lst'
 
 sed '1 i\<HTML><BODY>' $REGIONE'changeset.html'
-cat $REGIONEQ | grep "$IERI\|$OGGI" | grep changeset | awk ' { changeset=substr($0,index($0, "changeset")+11,8); print "<A HREF=https://overpass-api.de/achavi/?changeset="$changeset">"$changeset"</A>" }' | sort -u >> $REGIONE'changeset.html'
+cat $REGIONEQ | grep "$IERI\|$OGGI" | grep changeset | awk ' { $changeset=substr($0,index($0, "changeset")+11,8); print "<A HREF=https://overpass-api.de/achavi/?changeset="$changeset">"$changeset"</A>" }' | sort -u >> $REGIONE'changeset.html'
 echo "</BODY></HTML>" >> $REGIONE'changeset.html'
 
 # sorting and compacting changeset list
