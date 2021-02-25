f7f0e00b5ff74d48bae466cd148d71dde45e6e3b
cascafico <cascafico@gmail.com>
2021-02-25 10:14:25 +0100

    message edited

--- CAI_mods.sh
+++ CAI_mods.sh
@@ -1,9 +1,10 @@
 ######## start customization
 #AREACODE=3600000000 + <see areacodes in actions file>
 AREACODE=$1
-INTERVAL="yesterday"
 INTERVAL="1 hour ago"
 INTERVAL="10 days ago"
+INTERVAL="yesterday"
+INTERVAL="3 days ago"
 ######## end customization
 
 # dates for overpass syntax: 
@@ -15,9 +16,9 @@ OGGI=`date +"%Y-%m-%d"`
 
 
 # extracting overpass adiff differences
-curl -G 'http://overpass-api.de/api/interpreter' --data-urlencode 'data=[out:xml][timeout:300][adiff:"'$T0'","'$T1'"];area('$AREACODE')->.searchArea;(relation["operator"="Club Alpino Italiano"](area.searchArea);relation["operator"="CAI"](area.searchArea););(._;>;);out meta geom;' > $AREACODE'.osm'
+curl -G 'http://overpass-api.de/api/interpreter' --data-urlencode 'data=[out:xml][timeout:300][adiff:"'$T0'","'$T1'"];area('$AREACODE')->.searchArea;(relation["operator"~"^club alpino italiano",i](area.searchArea);relation["operator"="CAI"](area.searchArea););(._;>;);out meta geom;' > $AREACODE'.osm'
 
-echo "<HTML><BODY>Monitor process run on $T1<BR>Latest 24h changesets:<BR>" > $AREACODE'changeset.html'
+echo "<HTML><BODY>Monitor process run on $T1<BR>$INTERVAL changesets:<BR>" > $AREACODE'changeset.html'
 cat $AREACODE'.osm' | grep "$IERI\|$OGGI" | grep changeset | awk ' { $changeset=substr($0,index($0, "changeset")+11,8); print "<A HREF=https://overpass-api.de/achavi/?changeset="$changeset">"$changeset"</A><BR>" }' | sort -u >> $AREACODE'changeset.html'
 echo "</BODY></HTML>" >> $AREACODE'changeset.html'
 
