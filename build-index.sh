6060e9581b7480a98a44ef6c810993a5e9d84979
Giovanni <cascafico@gmail.com>
2021-02-25 14:40:21 +0100

    Update build-index.sh

--- build-index.sh
+++ build-index.sh
@@ -3,7 +3,8 @@ echo "<HTML><BODY>Monitor process run on $T1 <BR>" > index1.html
 for file in 36*changeset.html
 do
   echo "======" >> index1.html
-	cat ${file} >> index1.html
+  echo "<B>"$file"</B>" >> index1.html    
+  cat ${file} >> index1.html
 done
 
 echo "</BODY></HTML>" >> index1.html
