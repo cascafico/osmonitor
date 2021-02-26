cfe5cc17fcc926d4354769c31601368bf46c40b7
Giovanni <cascafico@gmail.com>
2021-02-26 11:00:05 +0100

    100 million

--- CAI_mods.sh
+++ CAI_mods.sh
@@ -93,5 +93,5 @@ then
 fi
 
 echo "<HTML><BODY>Monitor process run on $T1<BR>Changesets since $INTERVAL:<BR>" > $AREANAME'_changeset.html'
-cat $AREANAME'.osm' | grep "$IERI\|$OGGI" | grep changeset | awk ' { $changeset=substr($0,index($0, "changeset")+11,8); print "<A HREF=https://overpass-api.de/achavi/?changeset="$changeset">"$changeset"</A><BR>" }' | sort -u >> $AREANAME'_changeset.html'
+cat $AREANAME'.osm' | grep "$IERI\|$OGGI" | grep changeset | awk ' { $changeset=substr($0,index($0, "changeset")+11,9); print "<A HREF=https://overpass-api.de/achavi/?changeset="$changeset">"$changeset"</A><BR>" }' | sort -u >> $AREANAME'_changeset.html'
 echo "</BODY></HTML>" >> $AREANAME'_changeset.html'
