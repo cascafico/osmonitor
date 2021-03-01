35779270fae5c77275c634c9c4be076edf82c29f
Giovanni <cascafico@gmail.com>
2021-03-01 14:04:47 +0100

    Update build-index.sh

--- build-index.sh
+++ build-index.sh
@@ -5,5 +5,11 @@ do
   echo "<B>"$file"</B>" >> index1.html    
   cat ${file} >> index1.html
 done
-
 echo "</BODY></HTML>" >> index1.html
+
+echo "Index built on `date`<BR>" > index2.html
+for file in *_changeset.html
+do
+   echo "<A HREF="$file">"${file:0:10}"</A>" >> index2.html
+done
+echo "</BODY></HTML>" >> index2.html
